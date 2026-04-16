a331fcc40d9e84c4c69e4d2f014062b41d1fd4b8
RouaAyadi <roua.ayadi@insat.ucar.tn>
2026-04-16 20:53:53 +0100

    updates

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "db_user" {
+  default = "admin"
+}
+
+variable "db_password" {
+  type=string
+}
+
+variable "db_name" {
+  default = "mydatabase"
+}
+
+variable "db_port" {
+  default = 15432
+}
+
+variable "app_port" {
+  default = 8080
+}
